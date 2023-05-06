3d5d96f9fe0fa4e372dbb5fee7112a1bfea904bc
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-05-06 14:38:30 +0000

    change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../contexts/AuthContext'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
       <AuthProvider>
+        <Head>
+          <title>FoodieMakers</title>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </AuthProvider>
     </>
